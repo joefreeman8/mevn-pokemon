381ed70a12071d731041fec6d3013191b04a20ac
Joe Freeman <joefreeman8@gmail.com>
2024-01-14 12:29:40 +0000

    added custom logger for more indepth console feedback

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import 'dotenv/config'
-
+import logger from './lib/logger.js'
 import { connectDB } from './db/helpers.js'
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use('/', logger)
 
 
 const port = process.env.PORT || 4000
